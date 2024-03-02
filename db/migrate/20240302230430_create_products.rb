6b9658b9180b9793af3ef76df0ebe2d2ae097449
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-03-02 18:21:59 -0500

    Migrate the products table with the needed columns

--- db/migrate/20240302230430_create_products.rb
+++ db/migrate/20240302230430_create_products.rb
@@ -1,6 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
+      t.string :name
+      t.string :image
+      t.string :category
+      t.decimal :price, precision: 8, scale: 2
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_18_030203) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_230430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "category"
+    t.decimal "price", precision: 8, scale: 2
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
