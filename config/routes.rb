b4d79b8a6ffe67aa1f6c36b22efbc736bedf1299
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-03-11 11:29:34 -0500

    Create the users routes including auth, sessions and registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
+  devise_for :users, path: 'auth', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index, :show, :create, :destroy]
