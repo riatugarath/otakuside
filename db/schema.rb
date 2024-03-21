a0f2274b404b14800b109b968a57175d93c57f4a
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-03-21 08:33:25 -0500

    Migrate the new landing table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_190724) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_132747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_190724) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_carts_on_product_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "landings", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "category"
+    t.decimal "offer", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
