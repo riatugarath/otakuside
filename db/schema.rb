e269946ddb8db2ce13b63df09624723ef696a540
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-01 11:35:18 -0500

    Migrate the changes made in the products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_132747) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_161558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,10 +37,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_132747) do
     t.string "name"
     t.string "image"
     t.string "category"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "ori_price", precision: 8, scale: 2
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "sale_price", precision: 8, scale: 2
   end
 
   create_table "users", force: :cascade do |t|
