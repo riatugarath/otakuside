25a9eddab5c5980be913ec667813cc67189ccbe3
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-04-11 11:12:35 -0500

    Migrate the file to update the schema in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_161558) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_160711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_01_161558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "sale_price", precision: 8, scale: 2
+    t.string "size"
+    t.string "color"
   end
 
   create_table "users", force: :cascade do |t|
