7b9d353b2b568fa3c62c11a93fda9da920064866
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-06-14 06:51:31 -0500

    Add the create methos in the discount controller

--- app/controllers/api/V1/discounts_controller.rb
+++ app/controllers/api/V1/discounts_controller.rb
@@ -1,16 +1,30 @@
 class Api::V1::DiscountsController < ApplicationController
-    def index
-      @discounts = Discount.all
-      render json: @discounts
-    end
-  
-    def show
-      @discount = Discount.find_by(id: params[:id])
-      if @discount
-        render json: @discount, status: :ok
-      else
-        render json: { error: 'Discount not found' }, status: :not_found
-      end
-    end
-    
+  def index
+    @discounts = Discount.all
+    render json: @discounts
   end
+  
+  def show
+    @discount = Discount.find_by(id: params[:id])
+    if @discount
+      render json: @discount, status: :ok
+    else
+      render json: { error: 'Discount not found' }, status: :not_found
+    end
+  end
+  
+  def create
+    @discount = Discount.new(discount_params)
+    if @discount.save
+      render json: @discount, status: :created
+    else
+      render json: { error: 'Failed to create discount data' }, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def discount_params
+    params.require(:discount).permit(:name, :image, :current_price, :offer_price, :normal_price, :percentage)
+  end
+end
