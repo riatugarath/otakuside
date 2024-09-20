1e19b01067ea176dfb286ffeda3fc639ec1f711f
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-20 12:49:33 -0500

    Add test to make devide validations in the user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # Devise validations
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_presence_of(:password) }
+end
