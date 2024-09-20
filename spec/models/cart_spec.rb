1a0c19bcf9d3e4b64af6828a226b0e1660862e9b
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-20 12:29:03 -0500

    Add tests to try associations in the cart model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Cart, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:product) }
   end
+
+  # test the validations
+  describe 'Validations' do
+    # Validate the quantity to be present
+    it { should validate_presence_of(:quantity) }
+
+    # Validate the quantity is a integer and >= 1
+    it do
+      should validate_numericality_of(:quantity).
+        is_greater_than_or_equal_to(1).
+        only_integer
+    end
+
+    # Validate the cart has an user
+    it { should belong_to(:user) }
+
+    # Validate the cart has a product
+    it { should belong_to(:product) }
+  end
 end
