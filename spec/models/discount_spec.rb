d85de8783d23d2ab11c7aaf77d623e91c2aad65e
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-20 12:35:37 -0500

    Test to validate numeric values values in the discount model

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Discount, type: :model do
   it { should validate_length_of(:name).is_at_least(2) }
   it { should validate_length_of(:image).is_at_least(2) }
   it { should validate_length_of(:category).is_at_least(2) }
+
+  # Validate numeric values for prices y percentages
+  it { should validate_numericality_of(:offer_price).is_greater_than(0) }
+  it { should validate_numericality_of(:normal_price).is_greater_than(0) }
+  it { should validate_numericality_of(:percentage).is_greater_than(0) }
+  it { should validate_numericality_of(:current_price).is_greater_than(0) }
 end
