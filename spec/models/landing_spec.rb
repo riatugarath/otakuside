7cb30b37d7e991e1b2d25f4e028535b9b37181cd
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-20 12:38:56 -0500

    Test to validate presence and length in the landing model

---
+++ spec/models/landing_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Landing, type: :model do
+  # Validate presence
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:image) }
+  it { should validate_presence_of(:category) }
+
+  # Validamin length
+  it { should validate_length_of(:name).is_at_least(2) }
+  it { should validate_length_of(:image).is_at_least(2) }
+  it { should validate_length_of(:category).is_at_least(2) }
+end
