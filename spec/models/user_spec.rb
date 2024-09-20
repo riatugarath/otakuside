6146e55417ec9b96b37f93153b585e1cd24ee6ee
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-20 12:51:26 -0500

    Add test to make custom and association validations in the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_presence_of(:password) }
+
+  # Custom validations
+  it { should validate_presence_of(:name) }
+
+  # Association validation
+  it { should have_many(:carts).dependent(:destroy) }
 end
