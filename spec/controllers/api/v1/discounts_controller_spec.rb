af5638c2a8fab4563c5d031d4a16aa6fb5d9419b
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-21 13:34:26 -0500

    Add tests to verify the response when trying to show a discount

--- spec/controllers/api/v1/discounts_controller_spec.rb
+++ spec/controllers/api/v1/discounts_controller_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Api::V1::DiscountsController, type: :controller do
       expect(json_response.length).to eq(1) # check if there is a discount
     end
   end
+
+  describe "GET #show" do
+    context "when discount exists" do
+      it "returns a success response with the discount" do
+        get :show, params: { id: discount.id }
+        expect(response).to be_successful
+        json_response = JSON.parse(response.body)
+        expect(json_response['id']).to eq(discount.id)
+      end
+    end
+
+    context "when discount does not exist" do
+      it "returns a not found error" do
+        get :show, params: { id: 0 }
+        expect(response).to have_http_status(:not_found)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']).to eq('Discount not found')
+      end
+    end
+  end
 end
