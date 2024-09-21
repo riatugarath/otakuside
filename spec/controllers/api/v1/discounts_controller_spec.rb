b65414ec858af2db72b7e7679963ed470471a01e
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-21 13:35:57 -0500

    Add tests to check a discount creation

--- spec/controllers/api/v1/discounts_controller_spec.rb
+++ spec/controllers/api/v1/discounts_controller_spec.rb
@@ -32,4 +32,28 @@ RSpec.describe Api::V1::DiscountsController, type: :controller do
       end
     end
   end
+
+  describe "POST #create" do
+    context "with valid parameters" do
+      it "creates a new discount and returns a success response" do
+        expect {
+          post :create, params: { discount: { name: 'Black Friday', offer_price: 100, normal_price: 200, percentage: 50, current_price: 100, image: 'image_url', category: 'Tech' } }
+        }.to change(Discount, :count).by(1)
+
+        expect(response).to have_http_status(:created)
+        json_response = JSON.parse(response.body)
+        expect(json_response['name']).to eq('Black Friday')
+      end
+    end
+
+    context "with invalid parameters" do
+      it "returns an error response" do
+        post :create, params: { discount: { name: nil, offer_price: 100, normal_price: 200, percentage: 50, current_price: 100, image: 'image_url', category: 'Tech' } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response['error']).to eq('Failed to create discount data')
+      end
+    end
+  end
+
 end
