c4bfca9b928599268213e7d2873332fcea6c8f47
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-09-23 13:35:59 -0500

    Add tests to check the response when trying to show some landing content

--- spec/controllers/api/v1/landings_controller_spec.rb
+++ spec/controllers/api/v1/landings_controller_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Api::V1::LandingsController, type: :controller do
       expect(JSON.parse(response.body).size).to eq(1)
     end
   end
+
+  describe 'GET #show' do
+    context 'when the landing exists' do
+      it 'returns the landing and a success response' do
+        get :show, params: { id: landing.id }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['id']).to eq(landing.id)
+      end
+    end
+
+    context 'when the landing does not exist' do
+      it 'returns an error response' do
+        get :show, params: { id: -1 } # Un ID que no existe
+        expect(response).to have_http_status(:not_found)
+        expect(JSON.parse(response.body)['error']).to eq('Landing not found')
+      end
+    end
+  end
 end
